2e98107e5738262804f7c00a1a53ef8ec910f58d
bilalelkouazi <b.kouazi@gmail.com>
2019-03-07 21:40:54 +0100

    du replaced with stat

--- updspd.sh
+++ updspd.sh
@@ -2,18 +2,18 @@
 
 #
 # Estimates upload time of a given file
-# Use: updspd <file> <upload speed>
-# Note: upload speed is always mbps
-# example: upd /home/johndoe/mymovie.mp4 50
+# set upload speed at line 11
+# example: upd /home/johndoe/mymovie.mp4
 #
 
-#first argument is file, second speed in mbps
+#first argument is file
 file=$1
-uplspd=$2/8
+uplspd=50/8
 
 #find out size, convert to MB
 #is float number
-size= du $file | awk '{fileSizeMB = $1 / 1000; print fileSizeMB}'>tmp.log
+size= $[size + $((stat -c "%s" "$file"))]
 
+echo $size
 #divide size by time
 #estime=$size
